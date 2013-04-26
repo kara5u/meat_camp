816cd8e6d6a21f3cf32676b820b46028088541ea
Yutaka <ytk.karatsu@gmail.com>
2013-04-27 06:01:53 +0900

    feedをクロールしてDBにツッコミを入れるスクリプトDone

--- batch/fetch.py
+++ batch/fetch.py
@@ -4,6 +4,7 @@ import mechanize
 import BeautifulSoup
 import sqlite3
 import json
+import datetime
 
 def search_bot(url):
     br = mechanize.Browser()
@@ -21,8 +22,11 @@ def parse(xml):
         d = dict()
         #print '#'
         en_html = ""
+        pub_date = datetime.datetime.now()
+        url = ""
         for tag in item:
             if tag.__class__.__name__ == "Tag":
+                print tag.name.__str__()
                 if d.has_key(tag.name) == False:
                     d[tag.name] = list()
                 d[tag.name].append(tag.string)
@@ -30,11 +34,36 @@ def parse(xml):
                     c_soup = BeautifulSoup.BeautifulSoup(tag.string)
                     for a in c_soup.findAll('a'):
                         if a.string == u"原文へ":
-                            en_html = search_bot(a.get('href'))
+                            l = a.get('href')
+                            en_html = search_bot(l)
+                            url = l
+                elif tag.name == "pubdate":
+                    pub_date = datetime.datetime.strptime(
+                        tag.string.replace("+0000", ""), 
+                        "%a, %d %b %Y %H:%M:%S ")
+                #elif tag.name == "link":
+                #    print tag.string.__str__()
+                #    url = tag.string
         d["en"] = en_html
-        res.append(d)
+        #res.append(d)
+        db_insert(json.dumps(d), url, pub_date)
+
     return res
+
+def db_insert(feed, url, pubdate):
+    print pubdate
+    print url
+    conn = sqlite3.connect("../main.db")
+    c = conn.cursor()
+    c.execute("select * from feed where url='%s'" % url)
+    for row in c:
+        print "retrun"
+        return 
+    c.execute('insert into feed values (?, ?, ?)', (feed, url, pubdate))
+    conn.commit()
+    conn.close()
 
 
 res = json.dumps(parse(search_bot('http://jp.techcrunch.com/feed/')))
+#print res
 #print json.loads(res)
